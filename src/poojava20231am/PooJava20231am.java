2b8e18c5076bacc40f5a57faa20c0de7a2bdae0d
Carlos Pedraza <carlospedraza@usco.edu.co>
2023-02-08 10:47:34 -0500

    First commit

---
+++ src/poojava20231am/PooJava20231am.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package poojava20231am;
+
+/**
+ *
+ * @author Usuario
+ */
+public class PooJava20231am {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
