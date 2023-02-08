e2dfb7fa6fe663882d778fc12dc03a30fe04c68b
Carlos Pedraza <carlospedraza@usco.edu.co>
2023-02-08 10:51:16 -0500

    First commit

---
+++ src/firstapp/FirstApp.java
@@ -0,0 +1,16 @@
+package firstapp;
+/**
+ *
+ * @author Carlos Pedraza - 20231456123789
+ */
+public class FirstApp {
+
+    public static void main(String[] args) {
+        // Instrucción equivalente e Escribir
+        int valor=10;
+        System.out.println("Hola mundo desde Java!!");
+        System.out.println("Valor:"+valor);
+        
+    }
+    
+}
